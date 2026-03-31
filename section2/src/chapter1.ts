35d3980dfbcf49ca16330d309896dea09c7b789b
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2026-03-31 13:07:39 +0900

    feat: 원시 타입 & 리터럴 타입

---
+++ section2/src/chapter1.ts
@@ -0,0 +1,36 @@
+// * 원시 타입 *
+
+// number
+let num1: number = 123;
+let num2: number = -123;
+let num3: number = 0.123;
+let num4: number = -0.123;
+// 양의 무한대
+let num5: number = Infinity;
+// 음의 무한대
+let num6: number = -Infinity;
+// NaN = Not a Number
+let num7: number = NaN;
+
+// string
+let str1: string = 'Hello';
+let str2: string = 'World';
+let str3: string = `Hello, ${str1}!`;
+
+// boolean
+let bool1: boolean = true;
+let bool2: boolean = false;
+
+// null
+let null1: null = null;
+
+// undefined
+let undefined1: undefined = undefined;
+
+// * 리터럴 타입 *
+// 리터럴 -> 값
+let numA: 10 = 10;
+
+let strA: 'Hello' = 'Hello';
+
+let boolA: true = true;
