7fe573d52a7123d38ae2e096051f84c3466c6068
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2026-03-31 14:11:56 +0900

    feat: Any와 Unknown 타입

---
+++ section2/src/chapter6.ts
@@ -0,0 +1,26 @@
+// any
+// 특정 변수의 타입을 우리가 확실히 모를때
+let anyVar: any = 10;
+
+anyVar = 'Hello';
+anyVar = true;
+anyVar = {};
+anyVar = () => {};
+
+let num: number = 10;
+num = anyVar;
+
+// unknown
+// any와 비슷하지만, 좀 더 안전한 타입
+let unknownVar: unknown = 10;
+
+unknownVar = 'Hello';
+unknownVar = true;
+unknownVar = {};
+unknownVar = () => {};
+
+// num = unknownVar; // unknown 타입은 any와 달리, 다른 타입에 할당할 수 없음
+
+if (typeof unknownVar === 'number') {
+  num = unknownVar; // 타입 가드로 unknown 타입을 number로 좁혀서 할당 가능
+}
