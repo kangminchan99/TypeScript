d9a1a94a34e88e1f43e4b8b71b855f4d1aed6afd
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2026-04-06 15:38:43 +0900

    feat: 서로소 유니온 타입

---
+++ section3/src/chapter8.ts
@@ -0,0 +1,102 @@
+// 서로소 유니온 타입
+// 교집합이 없는 타입들로만 만든 유니온 타입을 말한다.
+
+type Admin = {
+  tag: 'ADMIN';
+  name: string;
+  kickCount: number;
+};
+
+type Member = {
+  tag: 'MEMBER';
+  name: string;
+  point: number;
+};
+
+type Guest = {
+  tag: 'GUEST';
+  name: string;
+  visitCount: number;
+};
+
+type User = Admin | Member | Guest;
+
+// Admin -> {name}님 현재까지 {kickCount}회 강퇴했습니다.
+// Member -> {name}님 현재까지 {point}점 모았습니다.
+// Guest -> {name}님 현재까지 {visitCount}회 방문했습니다.
+
+function login(user: User) {
+  switch (user.tag) {
+    case 'ADMIN':
+      console.log(`${user.name}님 현재까지 ${user.kickCount}회 강퇴했습니다.`);
+      break;
+    case 'MEMBER':
+      console.log(`${user.name}님 현재까지 ${user.point}점 모았습니다.`);
+      break;
+    case 'GUEST':
+      console.log(`${user.name}님 현재까지 ${user.visitCount}회 방문했습니다.`);
+      break;
+  }
+  // if (user.tag === 'ADMIN') {
+  //   console.log(`${user.name}님 현재까지 ${user.kickCount}회 강퇴했습니다.`);
+  // } else if (user.tag === 'MEMBER') {
+  //   console.log(`${user.name}님 현재까지 ${user.point}점 모았습니다.`);
+  // } else if (user.tag === 'GUEST') {
+  //   console.log(`${user.name}님 현재까지 ${user.visitCount}회 방문했습니다.`);
+  // }
+}
+
+// 복습겸 한가지 더 사례
+// 비동기 작업의 결과를 처리하는 객체
+
+type LoadingTask= {
+  state : 'LOADING';
+};
+
+type FailedTask = {
+  state: 'FAILED';
+  error: {
+    message: string;
+  };
+};
+
+type SuccessTask = {
+  state: 'SUCCESS';
+  response: {
+    data: string;
+  };
+}
+
+type AsyncTask = LoadingTask | FailedTask | SuccessTask;
+
+function processResult(task: AsyncTask) {
+  switch (task.state) {
+    case 'LOADING':
+      console.log('로딩 중...');
+      break;
+    case 'FAILED':
+      console.log(`실패: ${task.error.message}`);
+      break;
+    case 'SUCCESS':
+      console.log(`성공: ${task.response.data}`);
+      break;
+  }
+}
+
+const loading: AsyncTask = {
+  state: 'LOADING',
+};
+
+const failed: AsyncTask = {
+  state: 'FAILED',
+  error: {
+    message: '네트워크 오류',
+  },
+};
+
+const success: AsyncTask = {
+  state: 'SUCCESS',
+  response: {
+    data: '데이터',
+  },
+};
